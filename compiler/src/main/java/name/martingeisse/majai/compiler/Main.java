d2ec278cdf9003beea0d1b34aed2f1e0a6fcb040
Martin Geisse <martin.geisse@servicereisen.de>
2019-08-14 10:19:26 +0200

    ...

--- compiler/src/main/java/name/martingeisse/majai/compiler/Main.java
+++ compiler/src/main/java/name/martingeisse/majai/compiler/Main.java
@@ -11,24 +11,13 @@ import java.nio.charset.StandardCharsets;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		try {
-			try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(System.out, StandardCharsets.UTF_8)) {
+		File outputFolder = new File("out/majai");
+		outputFolder.mkdirs();
+		File outputFile = new File(outputFolder, "selftest.S");
+		try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+			try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8)) {
 				new Compiler(new ClassFileLoader(), "name/martingeisse/majai/payload/SelfTest", outputStreamWriter).compile();
 			}
-
-			File outputFolder = new File("out/majai");
-			outputFolder.mkdirs();
-			File outputFile = new File(outputFolder, "selftest.S");
-			try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
-				try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8)) {
-					new Compiler(new ClassFileLoader(), "name/martingeisse/majai/payload/SelfTest", outputStreamWriter).compile();
-				}
-			}
-
-		} catch (Throwable t) {
-			// unmix System.out and System.err on the IntelliJ console
-			Thread.sleep(1000);
-			throw t;
 		}
 	}
 
